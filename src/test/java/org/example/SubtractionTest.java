c01d1a5a97dce68352f793f227b406779b4f7157
JinYing24009209 <2817863922@qq.com>
2025-12-10 17:09:00 +0800

    subtration test

---
+++ src/test/java/org/example/SubtractionTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SubtractionTest {
+    @Test
+    void subtraction(){
+        assertEquals(10, )
+    }
+}
